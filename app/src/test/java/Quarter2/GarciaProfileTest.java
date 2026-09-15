efed5bf65c69dcd64d4ea47f3841db948a377eff
Pau_Shan <garciaseanjavierf@gmail.com>
2026-09-15 16:29:09 +0800

    done

---
+++ app/src/test/java/Quarter2/GarciaProfileTest.java
@@ -0,0 +1,20 @@
+package Quarter2;
+import org.junit.Test;
+public class GarciaProfileTest {
+
+@Test
+public void printMYprofile(){
+    String myName = "Garcia";
+    String petname1 = "bubu";
+    String petname2 = "dudu";
+    String favfood = "adobo";
+    int myAge = 19;
+
+    System.out.println("--- MY DIGITAL PROFILE ---");
+    System.out.println("Hello, my name is " + myName + "and I am " + myAge + " years old.");
+    System.out.println("I have two pets, " + petname1 + " and " + petname2 + ".");;;
+    System.out.println("If I could eat " + favfood + " everyday!");
+
+    }
+
+}
