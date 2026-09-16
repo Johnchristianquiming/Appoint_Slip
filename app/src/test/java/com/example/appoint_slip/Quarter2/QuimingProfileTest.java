597440a0196a3fdd9c01a3c7e8e904a59b62f708
Johnchristianquiming <johnchristianquiming@gmail.com>
2026-09-16 16:00:32 +0800

    QuimingProfile added

---
+++ app/src/test/java/com/example/appoint_slip/Quarter2/QuimingProfileTest.java
@@ -0,0 +1,19 @@
+package com.example.appoint_slip.Quarter2;
+import org.junit.Test;
+
+public class QuimingProfileTest {
+    @Test
+    public void myProfile() {
+        String myName = "Quiming";
+        String myFavPet = "Miming taba and liit";
+        String myFavPlace = "Intramuros and SM Manila";
+        String myFavFood = "Dinakdakan and Sisig";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello! my name is " + myName + "!! and I am " + myAge + " years old!");
+        System.out.println("I have two cats named " + myFavPet + ", and they are energetic!");
+        System.out.print("My favorite food is " + myFavFood + ", and my favorite places to go to always is " + myFavPlace + "!");
+    }
+
+}
